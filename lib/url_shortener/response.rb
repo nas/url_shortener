4cb0b0e43b3fe5a50b53b23360c2cde969c8e88b
nas <nas35_in@yahoo.com>
2009-11-19 06:40:22 +0000

    Add method missing to return values from the api response dynamically, also add
     * private methods to camelize strings.
     * add private method #base_element to use #attributes method 
       which will be mostly overridden in subclasses

--- lib/url_shortener/response.rb
+++ lib/url_shortener/response.rb
@@ -8,5 +8,33 @@ module UrlShortener
       @attributes = Hashie::Mash.new(result)
     end
     
+    def method_missing(method_name, *args)
+      element = camelize(method_name)
+      base_element.send(element.to_sym)
+    end
+    
+    private
+    
+    def camelize(string)
+      split_string_by_underscore = split_by_char(string, '_')
+      return split_string_by_underscore.first if split_string_by_underscore.size == 1
+      capitalized_parts = []
+      split_string_by_underscore.each_with_index do |word_part,index| 
+        next if index == 0
+        capitalized_parts << word_part.capitalize
+      end
+      camelized_string = capitalized_parts.unshift(split_string_by_underscore[0])
+      camelized_string.join('')
+    end
+    
+    def split_by_char(string, splitter)
+      operable_string = string.is_a?(String) ? string : string.id2name
+      operable_string.split(splitter)
+    end
+    
+    def base_element
+      attributes
+    end
+    
   end
 end
